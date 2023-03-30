08b517d8bf3845a9dc1b234e7118168dea6c59a5
david <david.antonio12@hotmail.com>
2023-03-29 23:17:22 -0500

    vite config file update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    write: true,
+    onEnd: () => {
+      fs.writeFile('./dist/CNAME', 'www.davidherediadev.com', (err) => {
+        if (err) throw err;
+        console.log('CNAME file has been created.');
+      });
+    },
+  },
+});
