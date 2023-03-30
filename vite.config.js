ad0af2fc6f31120e6879242c3b8b7e1ab46ab9d4
david <david.antonio12@hotmail.com>
2023-03-29 23:23:47 -0500

    vite config file restore

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import fs from 'fs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    write: true,
-    onEnd: () => {
-      fs.writeFile('./dist/CNAME', 'www.davidherediadev.com', (err) => {
-        if (err) throw err;
-        console.log('CNAME file has been created.');
-      });
-    },
-  },
-});
+})
