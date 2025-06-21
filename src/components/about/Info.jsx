b6dfb0f7a0524dadb57eb8ccd54ae10c813b5630
David <david.antonio12@hotmail.com>
2025-06-21 17:12:08 -0500

    refactor: update artifact version and about info

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,12 +6,12 @@ export const Info = () => {
             <div className="about-box">
             <i className="uil uil-award about-icon"></i>
                 <h3 className="about-title">Certificates</h3>
-                <span className="about-subtitle">8+</span>
+                <span className="about-subtitle">10+</span>
             </div>
             <div className="about-box">
             <i className="uil uil-briefcase-alt about-icon"></i>
                 <h3 className="about-title">Projects</h3>
-                <span className="about-subtitle">7+</span>
+                <span className="about-subtitle">50+</span>
             </div>
             <div className="about-box">
             <i className="uil uil-desktop-cloud-alt about-icon"></i>
